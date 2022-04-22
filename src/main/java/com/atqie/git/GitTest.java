360ed17ab3d8071b5efdbc53555d495bd521ed10
qie <git112@qie.com>
2022-04-22 09:15:43 +0800

    first commit

---
+++ src/main/java/com/atqie/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atqie.git;
+
+/**
+ * @Author 郄
+ * @Date 2022/4/22 9:13
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
